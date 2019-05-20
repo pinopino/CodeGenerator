bedc4d37ecd1fcee5ca0c8c789308c7bdc8ddf2f
pinopino <myhinata@126.com>
2019-05-20 17:10:48 +0800

    重构支持mysql-0.0.3

--- Console/Program.cs
+++ Console/Program.cs
@@ -11,71 +11,71 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Console
 {
     class Program
     {
-        class fetchinfo_sqlserver
+        public abstract class BaseParser
         {
-            public void fetch(string conn_str)
-            {
-                using (SQLServerManagement manage = new SQLServerManagement(conn_str))
-                {
-                    //var data = manage.Databases[dbcode].Tables;
-                    //// 解析数据库元数据
-                    //var parser = new MetaDataParser(config);
-                }
-            }
-        }
-
-        class fecthinfo_mysql
-        {
-            public void fetch(string conn_str)
-            {
-                using (MySqlConnection connection = new MySqlConnection(conn_str))
-                {
-                    //var CommandText = "SHOW TABLES;";
-                    //connection.Open();
-                    //List<string> TableNames = connection.Query<string>(CommandText).ToList();
-                }
-            }
-        }
-
-        abstract class parser_base
-        {
+            private readonly string _connstr;
             private ConsoleProgressBar _progress;
-            public parser_base(bool enableProgress = true)
+
+            public string ConnStr { get { return this._connstr; } }
+
+            public BaseParser(string connStr, bool enableProgress = true)
             {
+                _connstr = connStr;
                 if (enableProgress)
                     _progress = new ConsoleProgressBar(System.Console.CursorLeft, System.Console.CursorTop, 50, ProgressBarType.Character);
             }
 
-            public abstract void FetchMetadata(string conn_str);
-            protected abstract TableMetaData ParseTable(Table table);
-        }
+            public abstract Dictionary<string, TableMetaData> ParseMetadata();
 
-        class parser_sqlserver
-        {
-            private ConsoleProgressBar _progress;
-            public parser_sqlserver(bool enableProgress = true)
+            protected void ProgressPrint(long index, long total)
             {
-                if (enableProgress)
-                    _progress = new ConsoleProgressBar(System.Console.CursorLeft, System.Console.CursorTop, 50, ProgressBarType.Character);
+                if (_progress != null)
+                    _progress.Dispaly(Convert.ToInt32((index / (total * 1.0)) * 100));
             }
 
-            /// <summary>
-            /// 解析数据库元数据
-            /// </summary>
-            public List<TableMetaData> FetchMetadata(string conn_str)
+            protected string FindDBName(string connStr)
             {
-                using (SQLServerManagement manage = new SQLServerManagement(conn_str))
+                var db_name = string.Empty;
+                var cb = new DbConnectionStringBuilder(false);
+                cb.ConnectionString = connStr;
+                object database;
+                if (cb.TryGetValue("Initial Catalog", out database))
                 {
-                    var db_key = FindDBName(conn_str);
+                    db_name = database.ToString();
+                }
+                else
+                {
+                    if (cb.TryGetValue("Database", out database))
+                    {
+                        db_name = database.ToString();
+                    }
+                }
+
+                return db_name;
+            }
+        }
+
+        public class Parser_MSSQL : BaseParser
+        {
+            public Parser_MSSQL(string connStr, bool enableProgress = true)
+                : base(connStr, enableProgress)
+            { }
+
+            public override Dictionary<string, TableMetaData> ParseMetadata()
+            {
+                using (SQLServerManagement manage = new SQLServerManagement(base.ConnStr))
+                {
+                    var db_key = FindDBName(base.ConnStr);
                     var tables = manage.Databases[db_key].Tables;
-                    
-                    var ret = new List<TableMetaData>();
+
+                    var ret = new Dictionary<string, TableMetaData>();
                     // 解析表
                     var i = 0;
                     for (i = 0; i < tables.Count;)
@@ -87,14 +87,14 @@ namespace Console
                         {
                             ParseColumn(meta_table, col);
                         }
-                        ret.Add(meta_table);
+                        ret.Add(table.Name, meta_table);
 
                         // 打印进度
-                        ProgressPrint(_progress, ++i, tables.Count + 1);
+                        ProgressPrint(++i, tables.Count + 1);
                     }
 
                     // 打印进度
-                    ProgressPrint(_progress, ++i, tables.Count + 1);
+                    ProgressPrint(++i, tables.Count + 1);
 
                     return ret;
                 }
@@ -135,38 +135,86 @@ namespace Console
                     metaTable.Identity = meta_col;
                 }
             }
+        }
 
-            private void ProgressPrint(ConsoleProgressBar progress, long index, long total)
+        public class Parser_MYSQL : BaseParser
+        {
+            class FieldViewModel
             {
-                if (progress != null)
-                    progress.Dispaly(Convert.ToInt32((index / (total * 1.0)) * 100));
+                public string Field { get; set; }
+                public string Type { get; set; }
+                public dynamic Collation { get; set; }
+                public string Null { get; set; }
+                public string Key { get; set; }
+                public dynamic Default { get; set; }
+                public string Extra { get; set; }
+                public string Privileges { get; set; }
+                public string Comment { get; set; }
             }
 
-            private string FindDBName(string connStr)
+            public Parser_MYSQL(string connStr, bool enableProgress = true)
+                : base(connStr, enableProgress)
+            { }
+
+            public override Dictionary<string, TableMetaData> ParseMetadata()
             {
-                var db_name = string.Empty;
-                var cb = new DbConnectionStringBuilder(false);
-                cb.ConnectionString = connStr;
-                object database;
-                if (cb.TryGetValue("Initial Catalog", out database))
+                using (MySqlConnection connection = new MySqlConnection(base.ConnStr))
                 {
-                    db_name = database.ToString();
-                }
-                else
-                {
-                    if (cb.TryGetValue("Database", out database))
+                    var CommandText = "SHOW TABLES;";
+                    connection.Open();
+                    var TableNames = connection.Query<string>(CommandText).ToList();
+
+                    foreach (var name in TableNames)
                     {
-                        db_name = database.ToString();
+                        var table = new TableMetaData
+                        {
+                            Name = name,
+                            Columns = new List<ColumnMetaData>(),
+                            Comment = "",
+                            ExistPredicate = new List<ColumnMetaData>(),
+                            Identity = new ColumnMetaData(),
+                            PrimaryKey = new List<ColumnMetaData>(),
+                            WherePredicate = new List<ColumnMetaData>()
+                        };
+
+                        CommandText = $"SHOW FULL FIELDS FROM {name};"; // 得到表结构
+                        var result = connection.Query<FieldViewModel>(CommandText).ToList();
+                        foreach (var item in result)
+                        {
+                            item.Type = item.Type.Substring(0, item.Type.IndexOf("("));
+                            if (item.Type.Any(p => p.Equals("enum") || p.Equals("set")))
+                            {
+                                Print($"表{name}存在enum和set字段，暂无对应数据结构，跳过此表的生成！");
+                                break;
+                            }
+                            var ColumData = new ColumnMetaData
+                            {
+                                Comment = item.Comment,
+                                DbType = SQLMetaDataHelper.MapCsharpType(item.Type),
+                                HasDefaultValue = item.Default != null ? true : false,
+                                IsIdentity = item.Key == "PRI" ? true : false,
+                                IsPrimaryKey = item.Key == "PRI" ? true : false,
+                                Name = item.Field,
+                                Nullable = item.Null == "YES" ? true : false
+                            };
+
+                            if (item.Key == "PRI")
+                            {
+                                table.PrimaryKey.Add(ColumData);
+                                table.WherePredicate.Add(ColumData);
+                                table.ExistPredicate.Add(ColumData);
+                            }
+                            if (item.Extra == "auto_increment")
+                            {
+                                table.Identity = ColumData;
+                            }
+                            table.Columns.Add(ColumData);
+                        }
                     }
                 }
 
-                return db_name;
+                return new Dictionary<string, TableMetaData>();
             }
-        }
-
-        class parser_mysql
-        {
-
         }
 
         static void Main(string[] args)
@@ -182,54 +230,48 @@ namespace Console
                 Environment.Exit(0);
             }
 
-            ReCreateDB(conn_str, Encoding.GetEncoding("gb2312"));
+            // 根据配置文件确定生成mssql还是mysql
+            // todo...
 
-            var dbcode = FindDBName(conn_str);
             Print("解析数据库元数据...");
-            using (SQLServerManagement manage = new SQLServerManagement(conn_str))
+            var parser = new Parser_MSSQL(conn_str);
+            var meta_data = parser.ParseMetadata();
+
+            Print("解析完毕，生成中间配置文件...");
+            // 生成中间配置文件
+            var config_json_str = JsonConvert.SerializeObject(meta_data);
+            SQLMetaDataHelper.OutputConfig(config_json_str);
+
+            // 生成最终文件
+            Print("按 'y/Y' 继续生成最终操作类文件...");
+            var key = string.Empty;
+            do
             {
-                var config = new SQLMetaData();
-                SQLMetaDataHelper.InitConfig(config);
-                var data = manage.Databases[dbcode].Tables;
-                // 解析数据库元数据
-                var parser = new MetaDataParser(config);
-                parser.Parse(data);
-                Print("解析完毕，生成中间配置文件...");
-
-                // 生成中间配置文件
-                var config_json_str = JsonConvert.SerializeObject(config);
-                SQLMetaDataHelper.OutputConfig(config_json_str);
-
-                // 生成最终文件
-                Print("按 'y/Y' 继续生成最终操作类文件...");
-                var key = string.Empty;
-                do
+                key = System.Console.ReadLine();
+                if (key == "Y" || key == "y")
                 {
-                    key = System.Console.ReadLine();
-                    if (key == "Y" || key == "y")
-                    {
-                        // 生成DAL最终文件
-                        Print("生成DAL...");
-                        SQLMetaDataHelper.OutputDAL(config);
+                    // 生成DAL最终文件
+                    Print("生成DAL...");
+                    //SQLMetaDataHelper.OutputDAL(config);
 
-                        // 生成Model最终文件
-                        Print("生成Model...");
-                        SQLMetaDataHelper.OutputModel(config);
+                    // 生成Model最终文件
+                    Print("生成Model...");
+                    //SQLMetaDataHelper.OutputModel(config);
 
-                        // 生成Enum最终文件
-                        Print("生成Enum...");
-                        SQLMetaDataHelper.OutputEnum(config);
+                    // 生成Enum最终文件
+                    Print("生成Enum...");
+                    //SQLMetaDataHelper.OutputEnum(config);
 
-                        // 检测partial字段有效性
-                        Print("检测partial字段有效性...");
-                        SQLMetaDataHelper.DoPartialCheck(config);
+                    // 检测partial字段有效性
+                    Print("检测partial字段有效性...");
+                    //SQLMetaDataHelper.DoPartialCheck(config);
 
-                        Print("生成完毕！");
-                        break;
-                    }
-                    System.Console.WriteLine("按‘quit’退出");
-                } while (key != "quit");
-            }
+                    Print("生成完毕！");
+                    break;
+                }
+                System.Console.WriteLine("按‘quit’退出");
+            } while (key != "quit");
+
 
             Print("结束！");
             System.Console.Read();
