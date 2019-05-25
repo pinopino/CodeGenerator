8fbb2b459126b878e4cbc5764090b9ca3f4db940
pinopino <myhinata@126.com>
2019-05-25 20:19:20 +0800

    重构支持mysql-0.2.0-hotfix

--- Core/Generator/MSSql/ModelGenerator.cs
+++ Core/Generator/MSSql/ModelGenerator.cs
@@ -3,7 +3,6 @@ using Generator.Template;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Linq;
 
 namespace Generator.Core.MSSql
 {
@@ -60,96 +59,6 @@ namespace Generator.Core.MSSql
                                     string.IsNullOrWhiteSpace(_config.ModelConfig.BaseClass) ? string.Empty : (" : " + _config.ModelConfig.BaseClass),
                                     table.Name,
                                     sb1.ToString(),
-                                    sb2.ToString());
-            return str;
-        }
-
-        public override string Get_Joined_Class(JoinMapping join_info)
-        {
-            var mainTable = join_info.Table_Main.Name;
-            var subTable = join_info.Table_Sub.Name;
-            var alias = join_info.Sub_InnerName;
-
-            var sb1 = new StringBuilder();
-            var table_config = _tables[mainTable];
-            for (int i = 0; i < table_config.Columns.Count; i++)
-            {
-                var p = table_config.Columns[i];
-                if (p.Nullable && p.DbType != "string")
-                {
-                    sb1.AppendLine(string.Format("{0}{0}private {1}? _{2};", '\t', p.DbType, p.Name.ToLower()));
-                }
-                else
-                {
-                    sb1.AppendLine(string.Format("{0}{0}private {1} _{2};", '\t', p.DbType, p.Name.ToLower()));
-                }
-            }
-            sb1.AppendLine(string.Format("{0}{0}private {1} _{2};", '\t', subTable, subTable.ToLower()));
-
-            var sb2 = new StringBuilder();
-            for (int i = 0; i < table_config.Columns.Count; i++)
-            {
-                var p = table_config.Columns[i];
-                sb2.AppendLine(string.Format("{0}{0}/// <summary>", '\t'));
-                sb2.AppendLine(string.Format("{0}{0}/// {1}", '\t', p.Comment));
-                sb2.AppendLine(string.Format("{0}{0}/// </summary>", '\t'));
-                if (p.Nullable && p.DbType != "string")
-                {
-                    sb2.AppendLine(string.Format("{0}{0}public {1}? {2}", '\t', p.DbType, p.Name));
-                }
-                else
-                {
-                    sb2.AppendLine(string.Format("{0}{0}public {1} {2}", '\t', p.DbType, p.Name));
-                }
-                sb2.AppendLine(string.Format("{0}{0}{{", '\t'));
-                sb2.AppendLine(string.Format("{0}{0}{0}set {{ _{1} = value; }}", '\t', p.Name.ToLower()));
-                sb2.AppendLine(string.Format("{0}{0}{0}get {{ return _{1}; }}", '\t', p.Name.ToLower()));
-                sb2.AppendLine(string.Format("{0}{0}}}", '\t'));
-            }
-
-            table_config = _tables[subTable];
-            var sb3 = new StringBuilder();
-            for (int i = 0; i < table_config.Columns.Count; i++)
-            {
-                var p = table_config.Columns[i];
-                if (p.Nullable && p.DbType != "string")
-                {
-                    sb3.AppendLine(string.Format("{0}{0}{0}private {1}? _{2};", '\t', p.DbType, p.Name.ToLower()));
-                }
-                else
-                {
-                    sb3.AppendLine(string.Format("{0}{0}{0}private {1} _{2};", '\t', p.DbType, p.Name.ToLower()));
-                }
-            }
-
-            var sb4 = new StringBuilder();
-            for (int i = 0; i < table_config.Columns.Count; i++)
-            {
-                var p = table_config.Columns[i];
-                sb4.AppendLine(string.Format("{0}{0}{0}/// <summary>", '\t'));
-                sb4.AppendLine(string.Format("{0}{0}{0}/// {1}", '\t', p.Comment));
-                sb4.AppendLine(string.Format("{0}{0}{0}/// </summary>", '\t'));
-                if (p.Nullable && p.DbType != "string")
-                {
-                    sb4.AppendLine(string.Format("{0}{0}{0}public {1}? {2}", '\t', p.DbType, p.Name));
-                }
-                else
-                {
-                    sb4.AppendLine(string.Format("{0}{0}{0}public {1} {2}", '\t', p.DbType, p.Name));
-                }
-                sb4.AppendLine(string.Format("{0}{0}{0}{{", '\t'));
-                sb4.AppendLine(string.Format("{0}{0}{0}{0}set {{ _{1} = value; }}", '\t', p.Name.ToLower()));
-                sb4.AppendLine(string.Format("{0}{0}{0}{0}get {{ return _{1}; }}", '\t', p.Name.ToLower()));
-                sb4.AppendLine(string.Format("{0}{0}{0}}}", '\t'));
-            }
-
-            var str = string.Format(ModelTemplate.JOINED_CLASS,
-                                    "Joined" + mainTable,
-                                    "Joined" + mainTable,
-                                    subTable,
-                                    sb3.ToString(),
-                                    sb4.ToString(),
-                                    Environment.NewLine + sb1.ToString(),
                                     sb2.ToString());
             return str;
         }
--- Core/Templates/ModelTemplate.cs
+++ Core/Templates/ModelTemplate.cs
@@ -6,7 +6,6 @@ namespace Generator.Template
     {
         public readonly static string CLASS = File.ReadAllText(Path.Combine("Templates", "Model", "CLASS.txt"));
         public readonly static string CLASS_WITH_TRACE = File.ReadAllText(Path.Combine("Templates", "Model", "CLASS_WITH_TRACE.txt"));
-        public readonly static string ENTITY_CLASS = File.ReadAllText(Path.Combine("Templates", "Model", "ENTITY_CLASS.txt"));
-        public readonly static string JOINED_CLASS = File.ReadAllText(Path.Combine("Templates", "Model", "JOINED_CLASS.txt"));
+        public readonly static string ENTITY_CLASS = File.ReadAllText(Path.Combine("Templates", "Model", "ENTITY_CLASS.txt"));        
     }
 }
