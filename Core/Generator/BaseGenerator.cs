8bbb710e0af8aef08c86cf6b9b63a6ba29a279b6
pinopino <myhinata@126.com>
2019-05-29 21:07:18 +0800

    重构支持mysql-0.2.9

--- Core/Generator/BaseGenerator.cs
+++ Core/Generator/BaseGenerator.cs
@@ -216,81 +216,44 @@ namespace Generator.Core
         #endregion
 
         #region Exists
-        public string Get_Exists(string tableName)
-        {
-            var str1 = Get_Exists1(tableName);
-            var str2 = Get_Exists2(tableName);
-            return str1 + Environment.NewLine + str2;
-        }
-        protected abstract string Get_Exists1(string tableName);
-        protected abstract string Get_Exists2(string tableName);
+        public abstract string Get_Exists(TableMetaData table);
         #endregion
 
         #region Insert
-        public abstract string Get_Insert(string tableName);
+        public abstract string Get_Insert(TableMetaData table);
         #endregion
 
         #region Delete
-        public string Get_Delete(string tableName)
-        {
-            var str1 = Get_Delete1(tableName);
-            var str2 = Get_Delete2(tableName);
-            return str1 + Environment.NewLine + str2;
-        }
-        protected abstract string Get_Delete1(string tableName);
-        protected abstract string Get_Delete2(string tableName);
+        public abstract string Get_Delete(TableMetaData table);
         #endregion
 
         #region BatchDelete
-        public abstract string Get_BatchDelete(string tableName);
+        public abstract string Get_BatchDelete(TableMetaData table);
         #endregion
 
         #region Update
-        public string Get_Update(string tableName)
-        {
-            var str1 = Get_Update1(tableName);
-            var str2 = Get_Update2(tableName);
-            return str2 + Environment.NewLine + str1;
-        }
-        protected abstract string Get_Update1(string tableName);
-        protected abstract string Get_Update2(string tableName);
+        public abstract string Get_Update(TableMetaData table);
         #endregion
 
         #region Select
-        public string Get_GetModel(string tableName)
-        {
-            var str1 = Get_GetModel1(tableName);
-            var str2 = Get_GetModel2(tableName);
-            return str1 + Environment.NewLine + str2;
-        }
-        protected abstract string Get_GetModel1(string tableName);
-        protected abstract string Get_GetModel2(string tableName);
-        public abstract string Get_GetList(string tableName);
+        public abstract string Get_GetModel(TableMetaData table);
+        public abstract string Get_GetList(TableMetaData table);
         #endregion
 
         #region Page
-        public abstract string Get_Count(string tableName);
-        public abstract string Get_GetListByPage(string tableName);
+        public abstract string Get_Count(TableMetaData table);
+        public abstract string Get_GetListByPage(TableMetaData table);
         #endregion
 
         #region Joined
-        public string Get_Joined(JoinMapping join_info)
+        public abstract string Get_Joined(JoinMapping join_info);
+        #endregion
+
+        #region tail
+        public string Get_Tail(TableMetaData table)
         {
-            var sb = new StringBuilder();
-            sb.AppendLine();
-            sb.Append(Get_JoinedPage(join_info));
-            sb.AppendLine();
-            sb.AppendLine(Get_Joined1(join_info));
-            sb.AppendLine(Get_Joined2(join_info));
-            sb.Append(Get_Joined3(join_info));
-
-            return sb.ToString();
+            return "\t}}" + Environment.NewLine + "}";
         }
-
-        protected abstract string Get_Joined1(JoinMapping join_info);
-        protected abstract string Get_Joined2(JoinMapping join_info);
-        protected abstract string Get_Joined3(JoinMapping join_info);
-        protected abstract string Get_JoinedPage(JoinMapping join_info);
         #endregion
 
         protected bool IsKeyword(string colunm)
